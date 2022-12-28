960f3e76e9786cb7d46b49c05afb82ae331dc4b0
VladOstapchuk <vlad.ostapchuk@gmail.com>
2022-12-28 22:25:45 +0200

    new 3 task on loop's chapter First arrays

---
+++ src/homeworks/Arrays/numberOfOdds.java
@@ -0,0 +1,24 @@
+package homeworks.Arrays;
+
+public class numberOfOdds {
+
+    //Написать программу для подсчета количества
+    //нечетных чисел в нем.
+    public static void main(String[] args) {
+        int[] array = new int[] {0,5,2,4,7,1,3,19};
+        int number = 0;
+
+        for ( int i = 0; i < array.length; i++){
+
+            if (array[i] % 2 != 0) {
+                number++;
+            }
+        }
+
+        System.out.println(number);
+
+    }
+
+
+
+}
