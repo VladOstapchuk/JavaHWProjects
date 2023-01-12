fcea590e2937aa0f6fe442d6910fa89bb91526bd
ostapchukvo <vlad.ostapchuk@gmail.com>
2023-01-12 13:20:54 +0200

    task 1 - how many symbols in a string

--- src/homeworks/strings/howmanysymbols.java
+++ src/homeworks/strings/howmanysymbols.java
@@ -9,19 +9,27 @@ public class howmanysymbols {
         System.out.println("Please enter some string here:");
         String result = newString.nextLine();
         char[] fromString = result.toCharArray();
-        String sample = "b";
-        char[] sampleChar = sample.toCharArray();
+        char whatFind = 'b';
+        //String sample = "b";
+        //char[] sampleChar = sample.toCharArray();
         int resultNumber = 0;
+        int resultNumber2 = 0;
 
         for (int i = 0; i < fromString.length; i++) {
-            if(fromString[i] == sampleChar[0])
-            {
-                resultNumber ++;
+            if (fromString[i] == whatFind) {
+                resultNumber++;
+            }
+        }
+        for (char i: fromString) {
+            if (i == whatFind) {
+                resultNumber2++;
             }
         }
 
+
         System.out.println(Arrays.toString(fromString));
         System.out.println(resultNumber);
+        System.out.println(resultNumber2);
     }
 
 }
