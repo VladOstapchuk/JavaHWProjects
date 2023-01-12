f8f37d096a05a1c87bd382fe4ff830f21979ee0c
ostapchukvo <vlad.ostapchuk@gmail.com>
2023-01-12 10:47:54 +0200

    first Strings

---
+++ src/homeworks/strings/fromLectureTasks.java
@@ -0,0 +1,11 @@
+package homeworks.strings;
+
+import java.util.Arrays;
+
+public class fromLectureTasks {
+    public static void main(String[] args) {
+        String text = "Test Hi !";
+        byte[] a = text.getBytes();
+        System.out.println(Arrays.toString(a));
+    }
+}
